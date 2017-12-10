5da70bf6218ecc849caeaddeddef447ed66fbc40
nancy sin <nsin@sbcglobal.net>
2017-12-10 17:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,4 @@ function printBadges(arrayNames){
   }
 
 
-  function coinToss() {
-    returm Math.random() >=0.5;
-  }
-   function tailsNeverFails(){
-     let counter = 0;
-     while(coinToss()){
-        counter++
-   }
-
-   return `You got ${counter} tails in a row!`
-   }
+  
