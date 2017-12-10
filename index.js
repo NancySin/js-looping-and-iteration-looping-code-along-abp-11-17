2fcd59c76bdce1aaae56de1d7d9815880aa0cb15
nancy sin <nsin@sbcglobal.net>
2017-12-10 16:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function printBadges(arrayNames){
+  for (let i= 0; i < arrayNames.length; i++){
+    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+  }
+    return arrayNames
+  }
